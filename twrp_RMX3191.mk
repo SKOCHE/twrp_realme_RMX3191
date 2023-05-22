27a7113b9b7c5fe7167ae8734e15edbfa91097c4
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2023-05-23 00:21:40 +0700

    Add files via upload

---
+++ twrp_RMX3191.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := RMX3191
+PRODUCT_NAME := twrp_RMX3191
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := C25
+PRODUCT_MANUFACTURER := Realme
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31 \
+    ro.bootimage.build.date.utc=0 \
+    ro.build.date.utc=0
